2e0bc743e545ef6c0af9d76e6ec91a005451364b
ParveenJatoi <pjatoi973@gmail.com>
2024-06-05 08:56:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# AddFriend-0neClick" 
---
+++ main.js
@@ -0,0 +1,17 @@
+var isStatus = document.querySelector("h5");
+var btn = document.querySelector("#Add");
+var check = 0;
+btn.addEventListener("click", function () {
+    if (check == 0) {
+        isStatus.innerHTML = "Friends";
+        isStatus.style.color = "green";
+        btn.innerHTML = "Remove Friend";
+        check = 1;
+    }
+    else {
+        isStatus.innerHTML = "Stranger";
+        isStatus.style.color = "red";
+        btn.innerHTML = "Add Friend";
+        check = 0;
+    }
+});
